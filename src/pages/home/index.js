7b95a9c951cc3414857ae7fd854601d96a6ebd56
maneeshc1497 <maneeshmjh13108@gmail.com>
2022-01-07 11:05:17 +0530

    changes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     return (
         <UserContextProvider>
         <div className="home">
-        <NavBar/>
+        <NavBar/>        
         <CreatePost/>
         <Feed/>
         </div>
